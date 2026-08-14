2ca9e869ffcdaa577bd0c91808b1c577cec95ae4
wilohub <ronald.enriquez.lopez@gmail.com>
2026-08-14 09:26:43 -0500

    se cambio la ip

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,44 +1,45 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Tu IP local fija de la red actual y el puerto de NestJS
-const API_URL = 'http://192.168.180.115:3000/api/v1';
+// const API_URL = 'http://192.168.180.115:3000/api/v1';
+const API_URL = "http://192.168.151.247:3000/api/v1";
 
 const api = axios.create({
-    baseURL: API_URL,
-    timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: API_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const productService = {
-    /**
-     * Intento 1: Buscar el producto en la DB Local de NestJS
-     */
-    scanProductLocal: async (barcode: string) => {
-        try {
-            const cleanBarcode = barcode.trim();
-            const response = await api.get(`/products/scan/${cleanBarcode}`);
-            return response.data;
-        } catch (error: any) {
-            if (error.response) throw error.response.data;
-            throw new Error('Error al conectar con la DB Local');
-        }
-    },
+  /**
+   * Intento 1: Buscar el producto en la DB Local de NestJS
+   */
+  scanProductLocal: async (barcode: string) => {
+    try {
+      const cleanBarcode = barcode.trim();
+      const response = await api.get(`/products/scan/${cleanBarcode}`);
+      return response.data;
+    } catch (error: any) {
+      if (error.response) throw error.response.data;
+      throw new Error("Error al conectar con la DB Local");
+    }
+  },
 
-    /**
-     * Intento 2: Buscar el producto en la API externa de Open Food Facts (OFF)
-     */
-    scanProductOFF: async (barcode: string) => {
-        try {
-            const cleanBarcode = barcode.trim();
-            const response = await api.get(`/open-food-facts/${cleanBarcode}`);
-            return response.data;
-        } catch (error: any) {
-            if (error.response) throw error.response.data;
-            throw new Error('Error al conectar con Open Food Facts');
-        }
-    },
+  /**
+   * Intento 2: Buscar el producto en la API externa de Open Food Facts (OFF)
+   */
+  scanProductOFF: async (barcode: string) => {
+    try {
+      const cleanBarcode = barcode.trim();
+      const response = await api.get(`/open-food-facts/${cleanBarcode}`);
+      return response.data;
+    } catch (error: any) {
+      if (error.response) throw error.response.data;
+      throw new Error("Error al conectar con Open Food Facts");
+    }
+  },
 };
 
 export default api;
